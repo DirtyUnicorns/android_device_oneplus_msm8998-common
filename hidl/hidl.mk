9ec8ff76de9bde3a43c8d270dcbb0ad0bd4b3bf1
jumoog <kilianvonpflugk@gmail.com>
2017-12-24 23:35:42 +0000

    MSM8998: Re-enable healthd
    Change-Id: Ie69182783d071ef0130d812a3cc1146c2d833abf

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
