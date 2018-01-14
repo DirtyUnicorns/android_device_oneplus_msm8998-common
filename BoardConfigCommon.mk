6ecfe6492377d27818e05b2890603ac9e7a6ef90
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-14 13:18:31 +0100

    Add 15 sec WiFi driver probe delay
    Needs http://gerrit.dirtyunicorns.com/#/c/1723/
    
    Change-Id: Iccac9aaa67a7b543e2f79af50f13fd566f79a7a7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -262,9 +262,10 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
+#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+#WIFI_DRIVER_MODULE_NAME := "wlan"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_PROBE_DELAY := 15
 
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
