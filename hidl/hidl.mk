441c5e98f9830f595f09fcce4d95b215b0c54604
dianlujitao <dianlujitao@lineageos.org>
2018-02-12 16:01:34 +0100

    msm8998: Fork camera provider HAL
    Change-Id: Iacdfa6e1a98cefd480bb73416f96532340a3e554

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service.oneplus5 \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
