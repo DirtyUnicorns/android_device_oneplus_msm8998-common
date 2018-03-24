fe188d1d08f607f299846bc77467c1426870085e
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:26:10 -0700

    msm8998: Enable TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I9b80b810ea43b2743557b6368d6113f3065da029

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -19,6 +19,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus5 \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
