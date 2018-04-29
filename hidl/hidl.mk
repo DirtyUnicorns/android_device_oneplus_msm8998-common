70a7d5e8602eba2139cd8f338e075828ba310993
Josh Chasky <jchasky@gmail.com>
2018-04-29 18:43:42 -0400

    Fix camera autofocusing
    Revert "MSM8998: Remove camera provider HAL fork"
    
    This reverts commit 07c8d5f16b84f5038be5554344fb71d982839226.
    
    Change-Id: I0ff1ce4743176c0650ae4fd6b61fcb096c560725

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -18,7 +18,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service.oneplus5 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
