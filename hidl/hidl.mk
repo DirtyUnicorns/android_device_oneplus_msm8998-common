dbd951844044afe127c8de7f0c4c49295d0d96cd
Bruno Martins <bgcngm@gmail.com>
2018-07-14 01:42:41 -0400

    Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Iaf93665d7944386fcdeebc3820489a2392b113ca

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -20,9 +20,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus5 \
     camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    camera.device@3.2-impl
 
 # Display
 PRODUCT_PACKAGES += \
