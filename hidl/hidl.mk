307faeb753569cea7eb73f8afc70a9a9115c80ca
LuK1337 <priv.luk@gmail.com>
2018-07-14 01:44:51 -0400

    Do not explicitly include camera.device implementations
    camera.device@1.0-impl and camera.device@3.2-impl are dependencies
    of camera provider therefore there's no need to add them to PRODUCT_PACKAGES.
    
    Change-Id: I8b686c9c9f6db19ce55b881559c9ee2d28c8cada

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -18,9 +18,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.oneplus5 \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl
+    android.hardware.camera.provider@2.4-service.oneplus5
 
 # Display
 PRODUCT_PACKAGES += \
