258bee680b4fc0d2d216f44db186ed2a72a2ecf5
Josh Chasky <jchasky@gmail.com>
2018-10-03 08:05:01 -0400

    Inherit from oneplus/extras
    * Repo is needed for alert slider, gestures, etc.
    
    Change-Id: Ib9cc3984358fd5584d9c17e3b39197af012a64c9

--- common.mk
+++ common.mk
@@ -456,5 +456,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
-# Inherit from oppo-common
-#$(call inherit-product, device/oppo/common/common.mk)
+# Inherit from oneplus-extras
+$(call inherit-product, device/oneplus/extras/common.mk)
