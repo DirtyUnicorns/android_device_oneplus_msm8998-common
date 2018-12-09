93e069f958521fcfc923c8d921c41411684d69da
Pranav Vashi <neobuddy89@gmail.com>
2018-12-09 15:25:18 +0530

    msm8998: Add missing vndk libs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,9 +1,11 @@
 VNDK_SP_LIBRARIES := \
+    android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.common@1.0 \
     android.hardware.graphics.common@1.1 \
     android.hardware.renderscript@1.0 \
+    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     android.hidl.memory.token@1.0 \
     libRSCpuRef \
@@ -16,6 +18,8 @@ VNDK_SP_LIBRARIES := \
     libc++ \
     libcompiler_rt \
     libcutils \
+    libdexfile \
+    libft2 \
     libhardware \
     libhidlbase \
     libhidlmemory \
@@ -23,6 +27,7 @@ VNDK_SP_LIBRARIES := \
     libhwbinder \
     libion \
     liblzma \
+    libpng \
     libunwind \
     libunwindstack \
     libutils \
