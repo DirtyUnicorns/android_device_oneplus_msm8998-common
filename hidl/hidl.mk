b17f99b6f193f44815b0724416792dd796600cd6
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-24 14:45:57 +0100

    msm8998: rename Widevine HIDL service to v1.1
    Change-Id: I5e057c6368ace7d8b02e1d85029f5624b84b84ca

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.1-service.widevine
 
 # Fingerprint
 PRODUCT_PACKAGES += \
