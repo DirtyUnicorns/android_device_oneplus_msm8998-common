8cb1b3733a2536b1a09dfeba8c79e6e591e0837f
Nick Doherty <nickdoh81@gmail.com>
2019-01-08 21:20:23 -0500

    Remove adb workaround
    And another shout out to @Timo
    
    Change-Id: I758965b58ba97890e05f8c737cd0fcd99749fbe7

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -245,8 +245,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.controller=a800000.dwc3 \
     sys.usb.rndis.func.name=gsi \
-    sys.usb.rmnet.func.name=gsi \
-    sys.usb.ffs.aio_compat=1
+    sys.usb.rmnet.func.name=gsi
 
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
